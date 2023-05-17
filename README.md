a255b1821fd42bb7f83b2055eebbeab1ac1b5adf
Alexandre Pokhodoun <alexpokho@gmail.com>
2023-05-17 13:15:46 -0400

    new

---
+++ README.md
@@ -0,0 +1,7 @@
+# My react portfolio webapp
+
+![Screenshot](screenshot.png)
+
+### Demo: https://alexpokho.xyz
+
+A simple, single page, portfolio website built with React and MUI. Features navigation and intelligent app-bar hiding. Project and Hobby cards are dynamically created from data. 
---
+++ client/src/components/catalogue/category/underSole.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import CatalogueTemplate from '../catalogueTemplate';
+import Grid from '@mui/material/Grid';
+import { Container } from '@mui/system';
+import getCatalogueData from '../../../staticData/catalogueData'
+
+export default function UnderSole() {
+
+    let catalogueData = getCatalogueData()
+
+    return (
+
+        <Container>
+
+            <Typography gutterBottom variant="h4" component="div">
+                Under-sole canting
+            </Typography>
+
+            <br></br>
+            <Grid container spacing={3}>
+                {catalogueData.map(block => CatalogueTemplate(block))}
+            </Grid>
+            <br></br>
+        </Container>
+    );
+}
